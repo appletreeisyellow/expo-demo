42d455bec043355e9cf1f72cee882072f0069212
Chunchun <yccvivo@gmail.com>
2021-09-27 17:11:58 -0700

    add imagePicker to open a photo from local device

--- App.js
+++ App.js
@@ -1,19 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { Image, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import logo from './assets/logo.png';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  let openImagePickerAsync = async () => {
+    let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      alert("Permission to access camera roll is required!");
+      return;
+    }
+
+    let pickerResult = await ImagePicker.launchImageLibraryAsync();
+
+    if (pickerResult.cancelled === true) {
+      return;
+    }
+
+    setSelectedImage({ localUri: pickerResult.uri });
+  }
+
+  if (selectedImage !== null) {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={{ uri: selectedImage.localUri }}
+          style={styles.thumbnail}
+        />
+      </View>
+    )
+  }
+
   return (
     <View style={styles.container}>
-      <Image source={logo} style={styles.logo}/>
+      <Image source={{ uri: 'https://i.imgur.com/TkIrScD.png' }} style={styles.logo}/>
 
       <Text style={styles.instructions}>
         To share a photo from your phone with a friend, just press the button below!
       </Text>
 
       <TouchableOpacity
-        onPress={() => alert("Hello, world!")}
+        onPress={openImagePickerAsync}
         style={styles.button}
       >
         <Text style={styles.buttonText}>Pick a photo</Text>
@@ -48,5 +78,10 @@ const styles = StyleSheet.create({
     fontSize: 20,
     color: "#fff",
     padding: 10,
+  },
+  thumbnail: {
+    width: 300,
+    height: 300,
+    resizeMode: "contain",
   },
 });
